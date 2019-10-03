9120ae8dc5e87c6e622082ede0379664429549f6
Austin Lanari <austin@jumanji.io>
2019-10-02 21:33:15 -0700

    root

---
+++ subtree.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+### This file is for easily adding or pulling from subtrees
+
+### CMD = "add" or "pull"
+### PROJ = the name of the project in our emacs lib e.g. "magit" or "dash"
+### REMOTE = either fully qualified clone link or named remote
+CMD=$1
+PROJ=$2
+REMOTE=$3
+
+### Run the stuff! ALWAYS squash
+git subtree $CMD --prefix=lib/$PROJ $REMOTE --squash
