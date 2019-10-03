1158caa62d0a401322a0897517bb8fb0ebbd3676
Austin Lanari <austin@jumanji.io>
2019-10-02 21:57:09 -0700

    handle default tag; fetch before pull

--- subtree.sh
+++ subtree.sh
@@ -8,11 +8,18 @@
 CMD=$1
 PROJ=$2
 REMOTE=$3
+TAG=${4-"master"}
+
+echo $TAG
 
 git remote add -f $PROJ $REMOTE || true
+if [ $CMD = "pull" ]
+then
+    git fetch $PROJ $TAG
+fi
 
 ### Run the stuff! ALWAYS squash
-git subtree $CMD --prefix=lib/$PROJ $PROJ master --squash
+git subtree $CMD --prefix=lib/$PROJ $PROJ $TAG --squash
 
 if [ $CMD = "add" ]
 then
