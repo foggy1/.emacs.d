132ca14290bf20494297faacea962f88674a8637
Austin Lanari <austin@jumanji.io>
2020-05-02 23:46:32 -0700

    vue mode

--- init.el
+++ init.el
@@ -138,7 +138,7 @@
 
 
 (require 'js2-mode)
-(add-to-list 'auto-mode-alist '("\\.vue\\'" . js2-mode))
+;; (add-to-list 'auto-mode-alist '("\\.vue\\'" . js2-mode))
 (require 'rjsx-mode)
 (require 'libgit) ;; for magit
 (require 'yasnippet) ;; for lsp-mode with snippets on
@@ -209,6 +209,7 @@
 
 (require 'terraform-mode)
 (require 'transient) ;; for magit
+(require 'vue-mode)
 (require 'web-mode)
 (require 'with-editor) ;; for magit
 (require 'yaml-mode)
